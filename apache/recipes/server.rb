5f759a20d6a2e7c4a0b698f236a508a845d382c5
stellarsquall <stellarsquall@protonmail.ch>
2019-04-11 15:03:55 -0600

    initial commit

---
+++ apache/recipes/server.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: apache
+# Recipe:: server
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+package 'apache2'
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
+
+service 'apache2' do
+  action [:start, :enable]
+end
---
+++ apache/test/integration/default/server_test.rb
@@ -0,0 +1,20 @@
+# # encoding: utf-8
+
+# Inspec test for recipe apache::server
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe file('/var/www/html/index.html') do
+  it { should exist }
+  its(:content) { should contain 'Hello, world'}
+end
+
+describe upstart_service('apache2') do
+  it { should be_listening }
+  it { should be_enabled }
+end
